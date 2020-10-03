37f07a301e2bcb8769ef884fe2de5fd62153a82a
TheoryOfNekomata <allan.crisostomo@outlook.com>
2020-10-03 16:50:28 +0800

    Add publish script
    Set up publish script to configure to multiple repositories.

---
+++ publish.sh
@@ -0,0 +1,24 @@
+for branch in $(cat package.json | jq .publishing | jq -r keys[]) ; do
+  git checkout $branch
+
+  rawRepository=$(cat package.json | jq -r .publishing.$branch.repository)
+  repository=$(cat package.json | jq -r .publishing.$branch.repository)
+  currentRepository=$(cat package.json | jq -r .repository)
+
+  if [ $repository = $currentRepository ]; then
+    echo "$( jq --arg repository "$repository" '.repository = $repository' package.json )" > package.json
+    echo "Publishing to primary repository: $repository"
+    git push
+  elif [ $rawRepository = 'null' ]; then
+    echo "$branch has no repository"
+  else
+    echo "$branch is a mirror"
+    echo "$( jq --arg repository "$repository" '.repository = $repository' package.json )" > package.json
+    echo "Publishing to mirror repository: $repository"
+    git push --mirror $currentRepository
+  fi
+
+  registry=$(cat package.json | jq -r .publishing.$branch.publishConfig.registry)
+  echo $registry
+  npm publish --registry=$registry
+done
